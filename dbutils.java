401442175ebfdba2450738e8e1ba2fe2b8b8cf7c
keerthukairaa <keerthu5211@gmail.com>
2022-12-19 18:55:47 +0530

    all commit in stage

--- dbutils.java
+++ dbutils.java
@@ -2,3 +2,8 @@ hello all
 changing 1 time
 added line in master branch
 updating this line in dev branch
+testing stash command
+testing merge cmd
+test merge after commit
+test merge under dev branch
+
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
 new file trying to use -a option but it only works for existing files
+stash commnd test
